8e106719d60b2d379d87de15e18b60cb481536a0
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-03-12 18:48:44 +0200

    Task-08-done

--- js/task-07.js
+++ js/task-07.js
@@ -5,7 +5,5 @@ const textEl = document.querySelector('#text');
 inputEl.addEventListener('input', onInputRange);
 
 function onInputRange(event) {
-    let x = event.currentTarget.value;
-
     textEl.style.fontSize = `${event.currentTarget.value}px`;
 };
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
 
+const formEl = document.querySelector('form.login-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Всі поля повинні бути заповнені!")
+    }
+
+    else
+        console.log({ email:email.value, password:password.value });
+        event.currentTarget.reset();
+}
