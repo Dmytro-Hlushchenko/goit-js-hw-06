12be71c414265d110bf075194c3401a84f936f28
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-03-12 19:43:27 +0200

    Task-09-done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,21 @@
+
+
+const bodyEl = document.querySelector('body');
+const changeColorBtnEl = document.querySelector('button.change-color');
+const colorCodeEl = document.querySelector('span.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeColorBtnEl.addEventListener('click', changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = `${color}`;
+  colorCodeEl.textContent = `${color}`;
+}
+
