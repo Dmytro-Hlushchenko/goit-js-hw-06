e4679a6dbdd2fa116a94bc114af287211818ebb9
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-03-12 14:13:28 +0200

    Task-05-done

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+
+
+const textInputEl = document.querySelector('#name-input');
+const textOutputEl = document.querySelector('#name-output');
+
+textInputEl.addEventListener('input', onInput);
+
+function onInput() {
+    if (textInputEl.value === "") {
+        textOutputEl.textContent = 'Anonymous';
+    }
+    else {
+        textOutputEl.textContent = textInputEl.value;
+    }
+};
